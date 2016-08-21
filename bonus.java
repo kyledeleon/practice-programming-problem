3387ddca98acf5a5f95fd985e7245067650b8ea7
Kyle Jay De leon <kyle.rdeleon@yahoo.com>
2016-08-22 00:54:45 +0800

    Qualifying Exams

---
+++ bonus.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class bonus{
+public static void main(String[]args){
+Scanner s = new Scanner(System.in);
+for (int i=1;i<16;i++){
+	System.out.print(if(i%15==0)? "BingBong":if(i%5==0)? "Bong":if(i%3==0)? "Bing":i);
+
+
+}
+}
+
